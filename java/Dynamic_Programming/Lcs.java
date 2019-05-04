1a2c26edad235fa948debb8341f2d97c4f704da7
wunian12138 <37185083+wunian12138@users.noreply.github.com>
2019-05-04 09:11:38 +0800

    lcs problem

--- java/Dynamic_Programming/Lcs.java
+++ java/Dynamic_Programming/Lcs.java
@@ -1,5 +1,5 @@
 
-/* Solve the longest common substring problem using dynamic programming. */
+/* Solve the longest common substring, subsequence problem using dynamic programming. */
 public class Lcs {
     /* Return the length of the longest common substring of string str1, str2 */
     public static int longestCommonSubstring(String str1, String str2) {
@@ -28,8 +28,27 @@ public class Lcs {
         return res;
     }
 
+    /* Return the length of the longest common subsequence of A and B. */
+    public static int longestCommonSubsequence(int[] A, int[] B) {
+        int la = A.length, lb = B.length;
+        int[][] dp = new int[la+1][lb+1];
+        for (int i=1; i<la+1; i++) {
+            for (int j=1; j<lb+1; j++) {
+                if (A[i-1] == B[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[la][lb];
+    }
+
     public static void main(String[] args) {
-        String s1 = "abc", s2 = "abcbceaaaaaad";
+        String s1 = "abcd", s2 = "abcbceaaaaaad";
         System.out.println(Lcs.longestCommonSubstring(s1, s2));
+        int[] A = {1,3,4,5,6,7,7,8};
+        int[] B = {3,5,7,4,8,6,7,8,2};
+        System.out.println(Lcs.longestCommonSubsequence(A, B));
     }
 }
--- python/Dynamic_programming/lcs.py
+++ python/Dynamic_programming/lcs.py
@@ -1,5 +1,6 @@
 
-def lcs(str1:str, str2:str) -> int :
+
+def longestCommonSubstring(str1:str, str2:str) -> int :
     """ 
     Using dynamic programming to solve the longest 
     common substring problem. 
@@ -16,7 +17,26 @@ def lcs(str1:str, str2:str) -> int :
     
     return max(max(_) for _ in dp)
 
+def longestCommonSubsequence(A:list, B:list) -> int:
+    """
+    Return the length of the longest common subsequence
+    of A and B.
+    """
+    dp = [[0 for _ in range(len(B)+1)] for _ in range(len(A)+1)]
+    for i in range(1, len(A)+1):
+        for j in range(1, len(B)+1):
+            if A[i-1] == B[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    return dp[-1][-1]
+
 if __name__ == "__main__":
     s1 = "abc"
     s2 = "abababcdd"
-    print(lcs(s1, s2))
+    print(longestCommonSubstring(s1, s2))
+    A = [1,3,4,5,6,7,7,8]
+    B = [3,5,7,4,8,6,7,8,2]
+    print(longestCommonSubsequence(A, B))
+
+
